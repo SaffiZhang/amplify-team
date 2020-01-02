399c31a64d07f1ba2a654c9c3d0a92eca076ee01
Saffi Zhang <szhang@viqsolutions.com>
2020-01-02 15:02:03 -0500

    add typecript to function

--- amplify/backend/function/lambda/src/index.js
+++ amplify/backend/function/lambda/src/index.js
@@ -1,19 +1,8 @@
-/* Amplify Params - DO NOT EDIT
-You can access the following resource attributes as environment variables from your Lambda function
-var environment = process.env.ENV
-var region = process.env.REGION
-var authAmplifyteam9be10219UserPoolId = process.env.AUTH_AMPLIFYTEAM9BE10219_USERPOOLID
-var storageS3BucketName = process.env.STORAGE_S3_BUCKETNAME
-var apiAmplifyteamGraphQLAPIIdOutput = process.env.API_AMPLIFYTEAM_GRAPHQLAPIIDOUTPUT
-var apiAmplifyteamGraphQLAPIEndpointOutput = process.env.API_AMPLIFYTEAM_GRAPHQLAPIENDPOINTOUTPUT
-
-Amplify Params - DO NOT EDIT */
-
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = async (event = {}) => {
+    console.log('Hello World!');
+    const response = JSON.stringify(event, null, 2);
     return response;
 };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90c2Mtc3JjL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQWEsUUFBQSxPQUFPLEdBQUcsS0FBSyxFQUFFLFFBQWEsRUFBRSxFQUFnQixFQUFFO0lBQzNELE9BQU8sQ0FBQyxHQUFHLENBQUMsY0FBYyxDQUFDLENBQUM7SUFDNUIsTUFBTSxRQUFRLEdBQUcsSUFBSSxDQUFDLFNBQVMsQ0FBQyxLQUFLLEVBQUUsSUFBSSxFQUFFLENBQUMsQ0FBQyxDQUFDO0lBQ2hELE9BQU8sUUFBUSxDQUFDO0FBQ3BCLENBQUMsQ0FBQSJ9
