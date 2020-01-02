a6879de4602270715f4085d0e6d33d1b547255c8
Saffi Zhang <szhang@viqsolutions.com>
2020-01-02 14:24:49 -0500

    add api

---
+++ amplify/backend/function/lambda/src/index.js
@@ -0,0 +1,19 @@
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var authAmplifyteam9be10219UserPoolId = process.env.AUTH_AMPLIFYTEAM9BE10219_USERPOOLID
+var storageS3BucketName = process.env.STORAGE_S3_BUCKETNAME
+var apiAmplifyteamGraphQLAPIIdOutput = process.env.API_AMPLIFYTEAM_GRAPHQLAPIIDOUTPUT
+var apiAmplifyteamGraphQLAPIEndpointOutput = process.env.API_AMPLIFYTEAM_GRAPHQLAPIENDPOINTOUTPUT
+
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
